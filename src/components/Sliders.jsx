866edab5c108ba7fb20619cf8b438c0460af1107
Ozi <ozisadiqkasai@gmail.com>
2025-02-07 10:48:30 +0100

    stlying slider

--- src/components/Sliders.jsx
+++ src/components/Sliders.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 
 const Sliders = () => {
@@ -8,7 +9,9 @@ const Sliders = () => {
 
   useEffect(() => {
     axios
-      .get("https://fakestoreapi.com/products/category/women%27s%20clothing?limit=5")
+      .get(
+        'https://fakestoreapi.com/products/category/women%27s%20clothing?limit=5'
+      )
       .then((response) => {
         setSliders(response.data);
         setLoading(false);
@@ -24,31 +27,51 @@ const Sliders = () => {
   if (error) return <div>Error loading images</div>;
 
   return (
-    <div style={{display:'flex',gap:'1rem',flexWrap:'nowrap',width:'100%',paddingTop:'1rem'}}>
+    <Wrapper>
       {sliders.map((product) => (
-        <div
-          key={product.id} 
-          style={{
-            border: '1px solid red',
-            display: 'flex',
-            flexDirection:'column',
-             justifyContent: 'center',
-             alignItems: 'center',
-          }}
-        >
+        <Content key={product.id}>
           <img
             src={product.image}
             alt={product.title}
             style={{ width: '150px', height: 'auto' }}
           />
-          <div style={{margin:'0 auto',border:'1px solid yellow',width:'70%'}}>
-            <p style={{width:'100%',textAlign:'center',border:'1px solid black'}}>{product.title}</p>
-            <p style={{textAlign:'center'}}>${product.price}</p>
+          <div
+            style={{
+              margin: '0 auto',
+              border: '1px solid yellow',
+              width: '70%',
+            }}>
+            <p
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                border: '1px solid black',
+              }}>
+              {product.title}
+            </p>
+            <p style={{ textAlign: 'center' }}>${product.price}</p>
           </div>
-        </div>
+        </Content>
       ))}
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: nowrap;
+  width: 100%;
+  padding-top: 1rem;
+`;
+
+const Content = styled.div`
+  border: 1px solid var(--grey-100);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex:1;
+`;
 
 export default Sliders;
